ec4a318a1dc984e8114630c92faaa60f107cbaa6
LPHung <phuhung16820@gmail.com>
2022-06-14 03:25:55 +0700

    hardhat fund me

---
+++ hardhat.config.js
@@ -0,0 +1,61 @@
+require("dotenv").config()
+
+require("@nomiclabs/hardhat-etherscan")
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+require("hardhat-deploy")
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://eth-rinkeby"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKET_API_KEY || "key"
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            // gasPrice: 130000000000,
+        },
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+            blockComfirmations: 6,
+        },
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.8",
+            },
+            {
+                version: "0.6.6",
+            },
+        ],
+    },
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "ETH",
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        user: {
+            default: 1,
+        },
+    },
+}
---
+++ scripts/withdraw.js
@@ -0,0 +1,19 @@
+const { getNamedAccounts, ethers } = require("hardhat")
+
+const main = async () => {
+    const { deployer } = await getNamedAccounts
+    const fundMe = await ethers.getContract("FundMe", deployer)
+    console.log("Funding")
+    const transactionResponse = await fundMe.withdraw()
+    await transactionResponse.wait(1)
+    console.log("Got it back")
+}
+
+main()
+    .then(() => {
+        process.exit(0)
+    })
+    .catch((err) => {
+        console.log(err)
+        process.exit(1)
+    })
